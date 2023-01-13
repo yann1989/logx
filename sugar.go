ead8b62a6468271e02bfec40eaec382f032acecf
yann <yanf@bdatacloud.cn>
2023-01-13 13:52:08 +0800

    进一步封装sugar

--- sugar.go
+++ sugar.go
@@ -27,3 +27,59 @@ func (l *Logger) Panicf(template string, args ...interface{}) {
 func (l *Logger) Fatalf(template string, args ...interface{}) {
 	l.sugar.Fatalf(template, args...)
 }
+
+func (l *Logger) Debugw(msg string, keysAndValues ...interface{}) {
+	l.sugar.Debugw(msg, keysAndValues...)
+}
+
+func (l *Logger) Infow(msg string, keysAndValues ...interface{}) {
+	l.sugar.Infow(msg, keysAndValues...)
+}
+
+func (l *Logger) Warnw(msg string, keysAndValues ...interface{}) {
+	l.sugar.Warnw(msg, keysAndValues...)
+}
+
+func (l *Logger) Errorw(msg string, keysAndValues ...interface{}) {
+	l.sugar.Errorw(msg, keysAndValues...)
+}
+
+func (l *Logger) DPanicw(msg string, keysAndValues ...interface{}) {
+	l.sugar.DPanicw(msg, keysAndValues...)
+}
+
+func (l *Logger) Panicw(msg string, keysAndValues ...interface{}) {
+	l.sugar.Panicw(msg, keysAndValues...)
+}
+
+func (l *Logger) Fatalw(msg string, keysAndValues ...interface{}) {
+	l.sugar.Fatalw(msg, keysAndValues...)
+}
+
+func (l *Logger) Debugln(args ...interface{}) {
+	l.sugar.Debugln(args...)
+}
+
+func (l *Logger) Infoln(args ...interface{}) {
+	l.sugar.Infoln(args...)
+}
+
+func (l *Logger) Warnln(args ...interface{}) {
+	l.sugar.Warnln(args...)
+}
+
+func (l *Logger) Errorln(args ...interface{}) {
+	l.sugar.Errorln(args...)
+}
+
+func (l *Logger) DPanicln(args ...interface{}) {
+	l.sugar.DPanicln(args...)
+}
+
+func (l *Logger) Panicln(args ...interface{}) {
+	l.sugar.Panicln(args...)
+}
+
+func (l *Logger) Fatalln(args ...interface{}) {
+	l.sugar.Fatalln(args...)
+}
