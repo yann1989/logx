49eacdd6aa2eedbb8ba1a8da6c5433bf2acbef87
yann <yanf@bdatacloud.cn>
2023-01-13 11:38:26 +0800

    fix 格式化打印

--- sugar.go
+++ sugar.go
@@ -1,29 +1,29 @@
 package logx
 
 func (l *Logger) Infof(template string, args ...interface{}) {
-	l.sugar.Infof(template, args)
+	l.sugar.Infof(template, args...)
 }
 
 func (l *Logger) Debugf(template string, args ...interface{}) {
-	l.sugar.Debugf(template, args)
+	l.sugar.Debugf(template, args...)
 }
 
 func (l *Logger) Warnf(template string, args ...interface{}) {
-	l.sugar.Warnf(template, args)
+	l.sugar.Warnf(template, args...)
 }
 
 func (l *Logger) Errorf(template string, args ...interface{}) {
-	l.sugar.Errorf(template, args)
+	l.sugar.Errorf(template, args...)
 }
 
 func (l *Logger) DPanicf(template string, args ...interface{}) {
-	l.sugar.DPanicf(template, args)
+	l.sugar.DPanicf(template, args...)
 }
 
 func (l *Logger) Panicf(template string, args ...interface{}) {
-	l.sugar.Panicf(template, args)
+	l.sugar.Panicf(template, args...)
 }
 
 func (l *Logger) Fatalf(template string, args ...interface{}) {
-	l.sugar.Fatalf(template, args)
+	l.sugar.Fatalf(template, args...)
 }
